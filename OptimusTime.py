c53c016ecbe42c62c6c933798c1b9c5013c7b3f1
Sterling Lemon <sterlinglemon@gmail.com>
2019-11-16 16:10:15 -0700

    Working

--- OptimusTime.py
+++ OptimusTime.py
@@ -3,10 +3,11 @@ import logging
 import sys
 import os
 import threading
+import random
+import time
 from pynput import mouse
 from pynput import keyboard
 from PyQt5 import uic
-from PyQt5.QtWidgets import QApplication
 from PyQt5.QtCore import *
 
 class OptimusTime:
@@ -21,7 +22,54 @@ class OptimusTime:
         self.startedAt = datetime.datetime.now()
         self.SetupKeyboardHook()
         self.SetupMouseHook()
+<<<<<<< Updated upstream
         self.timer = threading.Timer(5, self.timerInterval)
+=======
+        self.timer = threading.Timer(2*1, self.timerInterval)
+        self.title = 'Optimus Time'
+        self.left = 10
+        self.top = 10
+        self.width = 250
+        self.height = 75
+        self.maximumHeight = 75
+        self.minimumHeight = 75
+        self.maximumWidth = 250
+        self.minimumWidth = 250
+        self.initUI()
+    
+    def initUI(self):
+        self.setWindowTitle(self.title)
+        self.setGeometry(self.left, self.top, self.width, self.height)
+        
+        startButton = QPushButton('Start', self)
+        startButton.setToolTip('Start tracking your effectivness')
+        startButton.move(10,40)
+        startButton.setMinimumWidth(120)
+        startButton.height = 32
+        startButton.clicked.connect(self.Start)
+
+        stopButton = QPushButton('Stop', self)
+        stopButton.setToolTip('Stop tracking your effectivness')
+        stopButton.move(125,40)
+        stopButton.setMinimumWidth(120)
+        stopButton.height = 32
+        stopButton.clicked.connect(self.Stop)
+        
+        self.apmLabel = QLabel('00032', self)
+        self.apmLabel.width = 91
+        self.apmLabel.height = 31
+        self.apmLabel.move(155,8)
+        self.apmLabel.setFont(QFont('Arial', 24))
+        self.apmLabel.setAlignment(Qt.AlignRight)
+
+        infoLabel = QLabel('Actions Per Minute', self)
+        infoLabel.width = 151
+        infoLabel.height = 51
+        infoLabel.setFont(QFont('Arial', 12))
+        infoLabel.move(15,15)
+        
+        self.show()
+>>>>>>> Stashed changes
 
     def GetActionCount(self):
         return self.keyCount + self.mouseClickCount + self.scrollCount
@@ -63,7 +111,7 @@ class OptimusTime:
         listener.start()
 
     def LowProductivityPrompt(self):
-        print("low prodcutivity tirgger")
+        print("low prodcutivity trigger")
         Dialog, DialogWindow = uic.loadUiType("Low Productivy Prompt.ui")
         dialogWindow = DialogWindow()
         dialog = Dialog()
@@ -71,7 +119,10 @@ class OptimusTime:
         dialogWindow.exec()
 
     def timerInterval(self):
-        if self.GetActionCount() < 30:
-            self.LowProductivityPrompt()
+        #if self.GetActionCount() < 10:
+        #self.LowProductivityPrompt()  
+        self.apmLabel.setText(str(self.GetActionCount()))
         self.resetActionCounts()
+        time.sleep(2)
+        self.timerInterval()
         print("Timer reset")
--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 import sys
 import logging
 import datetime
-from MainForm import MainForm
+from OptimusTime import OptimusTime
+from PyQt5 import uic
 from PyQt5.QtWidgets import QApplication
 
 # This is the entry-point for the application.
 if __name__ == '__main__':
     app = QApplication(sys.argv)
-    ex = MainForm()
+    ex = OptimusTime()
     sys.exit(app.exec_())
