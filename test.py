89a4ce060684bf97441418f46788406317a18a3e
Sterling Lemon <sterlinglemon@gmail.com>
2019-11-16 02:47:47 -0700

    Added tests

---
+++ test.py
@@ -0,0 +1,26 @@
+from lib.OptimusTime import OptimusTime
+
+# This is the entry-point for the application.
+if __name__ == '__main__':
+    # Instantiate the time optimizer
+    optimusTime = OptimusTime()
+
+    keyboardPrompt = input("Type some keys, scroll your mouse, and click a few times: ")
+    
+    if optimusTime.keyCount > 0:
+        print('Passed Keyboard Test')
+    else:
+        print('Failed Keyboard Test')
+
+    if optimusTime.mouseClickCount > 0:
+        print('Passed Mouse Test')
+    else:
+        print('Failed Mouse Test')
+
+    if optimusTime.scrollCount > 0:
+        print('Passed Scroll Test')
+    else:
+        print('Failed Scroll Test')
+
+    exit()
+    
